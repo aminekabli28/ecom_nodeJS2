0fe163c3fa00b51e1b00f8f7d199f7c678345117
amine kabli <aminekabli99@gmail.com>
2024-02-11 18:32:04 +0100

    second commit

--- config/database.js
+++ config/database.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const dbConect = () => {
   mongoose
-    .connect(`mongodb+srv://${process.env.DB_CONN}@4store.jsdpp45.mongodb.net/`)
+    .connect(
+      `mongodb+srv://aminekabli58:123456789123456789@4store.jsdpp45.mongodb.net/`
+    )
     .then(() => {
       console.log("connection successfuly");
     });
